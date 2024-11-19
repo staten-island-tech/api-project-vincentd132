a4a0f27321227b8987a0e04c2f9b926406fe3243
vincentd132 <vincentd103@nycstudents.net>
2024-11-19 14:00:31 -0500

    w

--- app/main.js
+++ app/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+async function getData() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api");
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch data");
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    alert(`Error: ${error.message}`);
+  }
+}
