c5276874d90dae7149897c1ee0be2d29431b2884
vincentd132 <vincentd103@nycstudents.net>
2024-11-25 13:59:32 -0500

    w

--- app/main.js
+++ app/main.js
@@ -2,14 +2,33 @@ import "./style.css";
 
 async function getData() {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api");
+    const response = await fetch("https://rickandmortyapi.com/api/character");
     if (response.status !== 200) {
       throw new Error("Failed to fetch data");
     } else {
       const data = await response.json();
-      console.log(data);
+      displayCharacter(data.results);
     }
   } catch (error) {
     alert(`Error: ${error.message}`);
   }
 }
+function displayCharacter(characters) {
+  const container = document.querySelector("#people");
+  container.innerHTML = "";
+
+  characters.forEach((character) => {
+    const characterCardHTML = `
+      <div class="character-card">
+        <img src="${character.image}" alt="${character.name}" class="character-image" />
+        <div class="character-info">
+          <h3>${character.name}</h3>
+          <p>${character.species} - ${character.status}</p>
+        </div>
+      </div>
+    `;
+    container.insertAdjacentHTML("beforeend", characterCardHTML);
+  });
+}
+
+getData();
